eb9e9b65dd9b8b63419652657f8cf896e235c3de
cvchavez2 <cvchavez92@gmail.com>
2021-05-06 11:46:47 -0600

    initial commit - spring dependencies included

---
+++ src/main/java/com/carloscognizant/todolistfems/TodoListFemsApplication.java
@@ -0,0 +1,13 @@
+package com.carloscognizant.todolistfems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListFemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListFemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloscognizant/todolistfems/TodoListFemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscognizant.todolistfems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListFemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
